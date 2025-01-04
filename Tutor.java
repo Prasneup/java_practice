57e91bb69efe8994e552d6fb35cc9b771415cce1
Prasneup <prasannaneupane723@gmail.com>
2025-01-04 07:43:33 +0545

    Added Practice6.java and other files

---
+++ Tutor.java
@@ -0,0 +1,12 @@
+//Java program to convert linked list to array
+import java.util.*;
+public class Tutor {
+    
+	public static <T> Object[] convertLinkedListToArray(LinkedList<T> linkedList) {
+      Object[] array = linkedList.toArray();
+      return array;
+
+
+    }
+    
+}
